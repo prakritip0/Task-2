665b5964e8078e315b3af94df778056ed1236fd8
Prakriti Pokharel <prakriti.pokharel@globalyhub.com>
2023-03-30 13:16:14 +0545

    chore: fix push data fn to fileData

--- api/joke.js
+++ api/joke.js
@@ -7,7 +7,7 @@ import { finalData } from "../data/finalData.js";
 export const getJoke = async (category, userName) => {
     const joke = await axios.get(`https://api.chucknorris.io/jokes/random?category=${category}`);
     console.log(chalk.green(joke.data.value));
-    finalData.joke.push = joke.data.value;
+    finalData.joke.push(joke.data.value);
 
 }
 
