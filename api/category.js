721624f5b58d68c0a515edf026156dbd38369c68
Prakriti Pokharel <prakriti.pokharel@globalyhub.com>
2023-04-03 14:25:42 +0545

    chore: move chosen category inside try block

--- api/category.js
+++ api/category.js
@@ -13,22 +13,18 @@ export const listCategories = async () => {
             console.log(chalk.green("[" + (i + 1) + "]") + " " + chalk.green(category));
         });
         console.log("\n" + chalk.bgGreen("Please enter one of the above categories to hear a joke!"));
+
+        const chosenCategory = prompt(">");
+        if (isNaN(Number(chosenCategory))) {
+            finalData.category = chosenCategory;
+            return chosenCategory;
+
+        } else {
+            finalData.category = categoryData.data[chosenCategory - 1];
+            return categoryData.data[chosenCategory - 1];
+
+        }
     } catch (error) {
         console.error("\n" + chalk.red("error fetching category") + "\n");
     }
-
-    const chosenCategory = prompt(">");
-    // console.log(Number(chosenCategory));
-    if (isNaN(Number(chosenCategory))) {
-        finalData.category = chosenCategory;
-        return chosenCategory;
-
-    } else {
-        finalData.category = categoryData.data[chosenCategory - 1];
-        return categoryData.data[chosenCategory - 1];
-
-    }
-
-
-    // return chosenCategory;
 }
